eccfb18b03a27fc6a9282cc77d1aea72decbaff5
hirooyasu <80202967+hirooki-ueno@users.noreply.github.com>
2021-04-20 15:15:19 +0900

    index.php追加

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+    /* 外部ファイルを読み込んでsession_start関数でデータをWebサーバーに保存 */
+    require_once("./dbconnect.php");
+    session_start();
+    if(!isset($_SESSION['username'])) {
+        header("Location: login.php");
+    }
+
+    $query = "SELECT * FROM login WHERE id=".$_SESSION['username']."";
+    $result = $mysqli->query($query);
+
+    if(!$result) {
+        print ('クエリーが失敗しました。'.$mysqli->error);
+        $mysqli->close();
+        exit;
+    }
+
+    while($row = $result->FETCH_ASSOC()) {
+        $username = $row['username'];
+        $email = $row['email'];
+    }
+
+    $result->close();
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ホーム -MusicHub</title>
+</head>
+<body>
+    <h1>ホーム</h1>
+</body>
+</html>
